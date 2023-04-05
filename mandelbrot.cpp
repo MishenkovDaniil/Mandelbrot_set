060b16b3aa652f1701f739270a57a78486e1ea31
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-04-05 19:30:57 +0300

    reconstructed code by functions

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -14,27 +14,37 @@ static const int WINDOW_HEIGHT = 1000;
 static const int RADIUS = 10;
 static const int N_MAX = 255;
 
+static const size_t MAX_STR_LEN = 20;
+
 static const float DX = (X_END - X_START) / WINDOW_WIDTH;
 static const float DY = (Y_START - Y_END) / WINDOW_HEIGHT;
 
+static const char *fps_font_file = "fps_font.ttf";
 
-void create_mandelbrot_image (void);
+void create_mandelbrot (void);
+void load_mandelbrot_pixels (sf::Uint8 *pixels);
+void load_fps_text (sf::Text *fps_text, sf::Font *font, const char *fps_file, sf::Time elapsed_time);
 
 
 int main ()
 {
-    create_mandelbrot_image ();
+    create_mandelbrot ();
 
     return 0;
 }
 
-void create_mandelbrot_image ()
+void create_mandelbrot ()
 {
     sf::RenderWindow window (sf::VideoMode (WINDOW_WIDTH, WINDOW_HEIGHT), "Mandelbrot");
     
     while (window.isOpen ())
     {
         sf::Event event;
+        sf::Font fps_font;
+        sf::Text fps_text;
+        sf::Image mandelbrot_img;
+        sf::Texture mandelbrot_texture;
+        sf::Sprite mandelbrot_sprite;
         
         while (window.pollEvent (event))
         {
@@ -46,77 +56,85 @@ void create_mandelbrot_image ()
 
         sf::Uint8 pixels[4*WINDOW_HEIGHT*WINDOW_WIDTH] = {0};
         assert (pixels);
-        sf::Image mandelbrot_img;
         
         sf::Clock clock;
-
-        float y_coord = Y_START;
-        int y_pixel = 0;
-
-        while (y_pixel < WINDOW_HEIGHT)
-        {
-            float x_coord = X_START;
-
-            for (int x_pixel = 0; x_pixel < WINDOW_WIDTH; ++x_pixel, x_coord += DX)
-            {
-                int n = 0;
-                float prev_x = x_coord;
-                float prev_y = y_coord;
-
-
-                for (int i = 0; i < N_MAX; ++i)
-                {
-                    float xx = prev_x * prev_x;
-                    float yy = prev_y * prev_y;
-                    float xy = prev_x * prev_y;
-                    
-                    if (xx + yy > RADIUS*RADIUS)
-                    {
-                        break;
-                    }
-
-                    prev_x = xx - yy + x_coord;
-                    prev_y = xy + xy + y_coord;
-
-                    ++n;
-                }
-
-                int idx = 4*(y_pixel*WINDOW_WIDTH + x_pixel);
-                if (n != N_MAX)
-                {
-                    pixels[idx] = (11*n)%N_MAX;
-                    pixels[idx + 1] = (7*n)%N_MAX;
-                    pixels[idx + 2] = (13*n)%N_MAX;
-                    // color.r = (11*n)%N_MAX; //11 55 53
-                    // color.g = (7*n)%N_MAX;  //19 23 17
-                    // color.b = (13*n)%N_MAX; //11 13 7 
-                }
-                pixels[idx + 3] = 255;
-            }
-
-            y_coord -= DY;
-            ++y_pixel;
-        }
+        load_mandelbrot_pixels (pixels);
         sf::Time elapsed_time = clock.getElapsedTime ();
 
-        sf::Font font;
-        font.loadFromFile ("fps_font.ttf");
-        char string[20] = "";
-        sprintf (string, "FPS: %lf", (double) 1 / (double)elapsed_time.asSeconds ());
-        sf::Text fps( string, font);
+        load_fps_text (&fps_text, &fps_font, fps_font_file, elapsed_time);
 
         mandelbrot_img.create (WINDOW_WIDTH, WINDOW_HEIGHT, (const sf::Uint8 *) pixels);
-
-        sf::Texture texture;
-        texture.loadFromImage (mandelbrot_img);
-
-        sf::Sprite sprite (texture);
+        mandelbrot_texture.loadFromImage (mandelbrot_img);
+        mandelbrot_sprite.setTexture (mandelbrot_texture);
 
         window.clear ();
-        window.draw (sprite);
-        window.draw (fps);
+        window.draw (mandelbrot_sprite);
+        window.draw (fps_text);
         window.display();
     }
 
+}
+
+void load_mandelbrot_pixels (sf::Uint8 *pixels)
+{
+    float y_coord = Y_START;
+    int y_pixel = 0;
+
+    while (y_pixel < WINDOW_HEIGHT)
+    {
+        float x_coord = X_START;
+
+        for (int x_pixel = 0; x_pixel < WINDOW_WIDTH; ++x_pixel, x_coord += DX)
+        {
+            int n = 0;
+            float prev_x = x_coord;
+            float prev_y = y_coord;
+
+            for (int iter = 0; iter < N_MAX; ++iter)
+            {
+                float xx = prev_x * prev_x;
+                float yy = prev_y * prev_y;
+                float xy = prev_x * prev_y;
+                float cur_radius = xx + yy;
+
+                if (cur_radius > RADIUS*RADIUS)
+                {
+                    break;
+                }
+
+                prev_x = xx - yy + x_coord;
+                prev_y = xy + xy + y_coord;
+
+                ++n;
+            }
+
+            int idx = 4*(y_pixel*WINDOW_WIDTH + x_pixel);
+            if (n != N_MAX)
+            {
+                pixels[idx] = (11*n)%N_MAX;
+                pixels[idx + 1] = (7*n)%N_MAX;
+                pixels[idx + 2] = (13*n)%N_MAX;
+                // color.r = (11*n)%N_MAX; //11 55 53
+                // color.g = (7*n)%N_MAX;  //19 23 17
+                // color.b = (13*n)%N_MAX; //11 13 7 
+            }
+            pixels[idx + 3] = 255;
+        }
+
+        y_coord -= DY;
+        ++y_pixel;
+    }
+}
+
+void load_fps_text (sf::Text *fps_text, sf::Font *font, const char *fps_file, sf::Time elapsed_time)
+{
+    font->loadFromFile (fps_file);
+    
+    char string[MAX_STR_LEN] = "";
+
+    sprintf (string, "FPS: %lf", (double) 1 / (double)elapsed_time.asSeconds ());
+    
+    fps_text->setString (string);
+    fps_text->setFont (*font);
 }
 
